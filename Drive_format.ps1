4f2f431f1ed86c82351ee5fd6569119e3b2c4a00
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-06-02 14:26:14 +0200

    Create Drive_format.ps1
    Drive format
    Drive naming

---
+++ Drive_format.ps1
@@ -0,0 +1,30 @@
+Clear-Host
+
+# New Attached Disk Details
+do {
+    $RawDisk = Get-Disk | Where-Object { $_.PartitionStyle -eq 'RAW' }
+
+    if (-not $RawDisk) {
+        Write-Host "Waiting for a new RAW disk to be added..." -ForegroundColor Yellow
+        Start-Sleep -Seconds 5
+    }
+} while (-not $RawDisk)
+
+Write-Host "Below are the new disk details" -ForegroundColor Yellow
+$RawDisk | Select-Object DiskNumber, PartitionStyle, HealthStatus, IsBoot, Size | Sort-Object DiskNumber | Format-Table -AutoSize
+
+# Prompt user to enter disk number
+$Number = Read-Host "Enter the disk number to be formatted and assign new drive letter"
+
+# Initialize disk with GPT format
+Initialize-Disk -Number $Number -PartitionStyle GPT
+
+
+$unitSize= Read-Host "Enter the Allocationunitsize(65536(64kb)/4096bytes):"
+# New partition and format disk with new drive letter
+$Format_Disk = New-Partition -DiskNumber $Number -UseMaximumSize -AssignDriveLetter | 
+                    Format-Volume -FileSystem NTFS -AllocationUnitSize $unitSize -Confirm:$false
+
+Write-Host "Total disk in server currently::" -ForegroundColor Green
+#Disk_Details
+Get-Volume |Select-Object DriveLetter,OperationalStatus,HealthStatus,FileSystemType,AllocationUnitSize,Size,SizeRemaining |Sort-Object DriveLetter|Format-Table -AutoSize
